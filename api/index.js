bc2e795390296af15eddf550a24b4bbddec19248
microproCDN <microprogram27@gmail.com>
2026-04-30 08:51:08 -0400

    v1.01

--- api/index.js
+++ api/index.js
@@ -1,11 +1,11 @@
 export const config = { runtime: "edge" };
 
-const CDN_URL1 = (process.env.CDN_URL1 || "");
-const CDN_URL2 = (process.env.CDN_URL2 || "");
-const CDN_URL3 = (process.env.CDN_URL3 || "");
-const CDN_URL4 = (process.env.CDN_URL4 || "");
-const CDN_URL5 = (process.env.CDN_URL5 || "");
-const CDN_URL6 = (process.env.CDN_URL6 || "");
+const CDN_URL1 = process.env.CDN_URL1;
+const CDN_URL2 = process.env.CDN_URL2;
+const CDN_URL3 = process.env.CDN_URL3;
+const CDN_URL4 = process.env.CDN_URL4;
+const CDN_URL5 = process.env.CDN_URL5;
+const CDN_URL6 = process.env.CDN_URL6;
 
 export default async function handler(req) {
   try {
@@ -28,12 +28,13 @@ export default async function handler(req) {
 	const sfind2 = req.url.slice(req.url.indexOf("_"));
 
 	let url = null;
-	if(sfind2 == '_1' || sfind2 == '') url = (sfind === -1 ? CDN_URL1 + "/" : CDN_URL1 + req.url.slice(sfind).replace(sfind2,""));
+	if(sfind2 == '_1') url = (sfind === -1 ? CDN_URL1 + "/" : CDN_URL1 + req.url.slice(sfind).replace(sfind2,""));
 	if(sfind2 == '_2') url = (sfind === -1 ? CDN_URL2 + "/" : CDN_URL2 + req.url.slice(sfind).replace(sfind2,""));
 	if(sfind2 == '_3') url = (sfind === -1 ? CDN_URL3 + "/" : CDN_URL3 + req.url.slice(sfind).replace(sfind2,""));
 	if(sfind2 == '_4') url = (sfind === -1 ? CDN_URL4 + "/" : CDN_URL4 + req.url.slice(sfind).replace(sfind2,""));
 	if(sfind2 == '_5') url = (sfind === -1 ? CDN_URL5 + "/" : CDN_URL5 + req.url.slice(sfind).replace(sfind2,""));
 	if(sfind2 == '_6') url = (sfind === -1 ? CDN_URL6 + "/" : CDN_URL6 + req.url.slice(sfind).replace(sfind2,""));
+	if(sfind2 == '') url = (sfind === -1 ? CDN_URL1 + "/" : CDN_URL1 + req.url.slice(sfind));
 	
 	if(url) {
 		return await fetch(url, {
